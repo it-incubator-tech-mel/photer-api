82748ae66b71d42209ec8d9d2bbec9dc56cec2bc
Vladimir <vladimirleonov789@gmail.com>
2025-02-10 21:27:44 +0300

    feat: add and configure global pipe

--- apps/gateway/src/setup/app.setup.ts
+++ apps/gateway/src/setup/app.setup.ts
@@ -1,8 +1,10 @@
 import { globalPrefixSetup } from './global-prefix.setup';
 import { INestApplication } from '@nestjs/common';
 import { exceptionFiltersSetup } from './exception-filters.setup';
+import { pipesSetup } from './pipes.setup';
 
 export function appSetup(app: INestApplication) {
+  pipesSetup(app);
   globalPrefixSetup(app);
   exceptionFiltersSetup(app);
 }
