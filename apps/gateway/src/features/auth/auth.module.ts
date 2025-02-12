bfe3d801e9d9b758a8121d32acab2d38f13b40b2
Vladimir <vladimirleonov789@gmail.com>
2025-02-12 23:16:21 +0300

    feat: add CryptoModule

--- apps/gateway/src/features/auth/auth.module.ts
+++ apps/gateway/src/features/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { AuthController } from './api/auth.controller';
 import { MailerModule } from '../../core/services/mailler/mailer.module';
+import { CryptoModule } from '../../core/services/crypto/crypto.module';
 
 @Module({
-  imports: [CqrsModule, MailerModule],
+  imports: [CqrsModule, MailerModule, CryptoModule],
   controllers: [AuthController],
   exports: []
 })
