f6d37e582b9c79062832385f10a1bd8423d02909
Vladimir <vladimirleonov789@gmail.com>
2025-02-16 12:51:07 +0300

    docs(swagger): add API docs for RegistrationDto

--- apps/gateway/src/features/auth/api/dto/registration.dto.ts
+++ apps/gateway/src/features/auth/api/dto/registration.dto.ts
@@ -1,16 +1,32 @@
-import { IsBoolean, IsEmail, IsNotEmpty, IsString, Length, Matches } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 import { Trim } from '../../../../core/decorators/trim';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegistrationDto {
+    @ApiProperty({
+        minLength: 6,
+        maxLength: 30,
+        pattern: '^[a-zA-Z0-9_-]*$',
+        description: 'must be unique',
+    })
     @Length(6, 30, { message: 'Username must be between 6 and 30 characters' })
-    @Matches(/^[a-zA-Z0-9_-]{6,30}$/, { message: 'Username can only contain letters, numbers, underscores, and hyphens, and must be between 6 and 30 characters' })
+    @Matches(/^[a-zA-Z0-9_-]*$/, { message: 'Username can only contain letters, numbers, underscores, and hyphens' })
     username: string;
 
+    @ApiProperty({
+        pattern: '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+        example: 'user@example.com',
+        description: 'must be unique'
+    })
     @Trim()
     @IsNotEmpty({ message: 'Email is required' })
     @IsEmail({}, { message: 'Invalid email format' })
     email: string;
 
+    @ApiProperty({
+        minLength: 6,
+        maxLength: 20,
+    })
     @IsString()
     @Trim()
     @IsNotEmpty({ message: 'Password is required' })
@@ -20,7 +36,7 @@ export class RegistrationDto {
     })
     password: string;
 
-    @IsBoolean()
-    @IsNotEmpty({ message: 'Consent of service is required' })
-    consentOfService: boolean;
+    // @IsBoolean()
+    // @IsNotEmpty({ message: 'Consent of service is required' })
+    // consentOfService: boolean;
 }
