6b39440d1658fe8942d687fa5806bbb82cc923ce
Vladimir <vladimirleonov789@gmail.com>
2025-02-16 12:12:00 +0300

    refactor: app.module.ts

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,10 +1,8 @@
 import { configModule } from '../../common/config/config.module';
-import { Module } from '@nestjs/common';
-import { CoreConfig } from './core/config/core.config';
-import { ConfigService } from '@nestjs/config';
 import { AuthModule } from './features/auth/auth.module';
 import { DeviceModule } from './features/devices/device.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
