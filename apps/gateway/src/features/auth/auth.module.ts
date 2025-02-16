703a0086aa436160916f2771911029d0d5e5ed5d
Vladimir <vladimirleonov789@gmail.com>
2025-02-16 13:39:29 +0300

    feat: register registration.use-case.ts in auth.module.ts

--- apps/gateway/src/features/auth/auth.module.ts
+++ apps/gateway/src/features/auth/auth.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { AuthController } from './api/auth.controller';
 import { MailerModule } from '../../core/services/mailler/mailer.module';
 import { CryptoModule } from '../../core/services/crypto/crypto.module';
+import { RegistrationUseCase } from './application/use-cases/registration.use-case';
+
+const useCases: Provider[] = [RegistrationUseCase];
 
 @Module({
   imports: [CqrsModule, MailerModule, CryptoModule],
   controllers: [AuthController],
+  providers: [...useCases],
   exports: []
 })
 export class AuthModule {}
