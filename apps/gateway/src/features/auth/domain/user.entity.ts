3f61a84867d0b4833e83231cf1b6b3e65219d131
Vladimir <vladimirleonov789@gmail.com>
2025-02-16 20:46:58 +0300

    refactor: user model

---
+++ apps/gateway/src/features/auth/domain/user.entity.ts
@@ -0,0 +1,22 @@
+import { User } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserEntity implements User {
+    @ApiProperty()
+    id: number;
+
+    @ApiProperty()
+    username: string;
+
+    @ApiProperty()
+    password: string;
+
+    @ApiProperty()
+    email: string;
+
+    @ApiProperty()
+    createdAt: Date;
+
+    @ApiProperty()
+    updatedAt: Date;
+}
