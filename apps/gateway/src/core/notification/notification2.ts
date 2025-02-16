22c7ac28cdc98ad504f808ae39f66eb6b536f543
Vladimir <vladimirleonov789@gmail.com>
2025-02-16 13:24:42 +0300

    feat: add notification

---
+++ apps/gateway/src/core/notification/notification2.ts
@@ -0,0 +1,27 @@
+export class Notification<T = null> {
+  constructor(data: T | null = null) {
+    this.data = data;
+  }
+
+  messages: {message: string, key: string}[] = [];
+  code = 0;
+  data: T | null = null;
+
+  hasError() {
+    return this.code !== 0;
+  }
+
+  addError(
+    message: string,
+    key: string,
+    //code: number | null = null,
+    code?: number,
+  ) {
+    this.code = code ?? 1;
+    this.messages.push({message, key});
+  }
+
+  addData(data: T) {
+    this.data = data;
+  }
+}
