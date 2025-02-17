2a51b31901b76622a726e4f22b86722dd995642a
Vladimir <vladimirleonov789@gmail.com>
2025-02-17 22:53:29 +0300

    refactor: registration-email-template.ts message

--- apps/gateway/src/core/services/mailler/email-templates/registration-email-template.ts
+++ apps/gateway/src/core/services/mailler/email-templates/registration-email-template.ts
@@ -1,7 +1,7 @@
 export const registrationEmailTemplate = (confirmationCode: string): string => {
   return `
          <h1>Thanks for your registration</h1>
-         <p>To finish registration please follow the link below:
+         <p>To finish registration please follow the link:
              <a href='https://somesite.com/confirm-email?code=${confirmationCode}'>complete registration</a>
          </p>
     `;
