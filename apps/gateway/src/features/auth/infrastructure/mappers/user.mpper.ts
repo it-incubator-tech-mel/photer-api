dfbf6b50d89ba2227dab56de46b927301339c18a
Vladimir <60917839+vladimirleonov@users.noreply.github.com>
2025-03-07 14:35:49 +0300

    fix: user.mpper.ts

--- apps/gateway/src/features/auth/infrastructure/mappers/user.mpper.ts
+++ apps/gateway/src/features/auth/infrastructure/mappers/user.mpper.ts
@@ -2,6 +2,6 @@ import { AuthMeOutputDto } from '../../api/dto/output/auth-me.dto';
 
 export class UserMapper {
   static toAuthMeOutput(user): AuthMeOutputDto {
-    return new AuthMeOutputDto(user.getId(), user.getEmail());
+    return new AuthMeOutputDto(user.id, user.email);
   }
 }
