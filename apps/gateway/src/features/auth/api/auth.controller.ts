0988ea6baad7880bcd9fbe4e7e9982523b0278dd
Vladimir <vladimirleonov789@gmail.com>
2025-03-19 19:46:41 +0300

    feat: add swagger for 404 doc in create password-recovery-resending method

--- apps/gateway/src/features/auth/api/auth.controller.ts
+++ apps/gateway/src/features/auth/api/auth.controller.ts
@@ -299,6 +299,20 @@ export class AuthController {
   @Post('password-recovery-resending')
   @ApiOperation({ summary: 'Resend password recovery link via email' })
   @ApiResponse({ status: 204, description: 'Email with a new recovery link has been sent' })
+  @ApiResponse({
+    status: 400,
+    description: 'If the inputModel has incorrect value (for incorrect password length) or RecoveryCode is incorrect or expired',
+    type: APIErrorResult,
+    content: {
+      'application/json': {
+        example: {
+          statusCode: 400,
+          message: 'Validation failed',
+          errorsMessages: [],
+        },
+      },
+    },
+  })
   @ApiResponse({ status: 404, description: 'If user with this email does not exist' })
   @ApiResponse({ status: 429, description: 'More than 5 attempts from one IP-address during 10 seconds' })
   @HttpCode(HttpStatus.NO_CONTENT)
