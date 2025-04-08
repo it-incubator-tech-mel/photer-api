f5f2e1d81f34e06d00ffeeccc4dd3185f73d9d67
Maksim Shapovalov <142093626+Maksim-Shapovalov@users.noreply.github.com>
2025-04-08 22:38:01 +0300

    Delete prisma/migrations directory
    delete current migrations

--- prisma/migrations/20250212185958_add_length_checks_in_user_model/migration.sql
+++
@@ -1,6 +0,0 @@
--- add checks in user model
-ALTER TABLE "User"
-  ADD CONSTRAINT "check_username_length" CHECK (length(username) >= 3);
-
-ALTER TABLE "User"
-  ADD CONSTRAINT "check_password_length" CHECK (length(password) >= 6);
--- prisma/migrations/20250404192049_/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "PostStatusType" AS ENUM ('private', 'public');
-
--- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "status" "PostStatusType" NOT NULL DEFAULT 'public';
