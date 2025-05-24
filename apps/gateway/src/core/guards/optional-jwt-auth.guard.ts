38b06dca2e0c49fa7e752ef1ce4bcb1d536a1a5f
Vladimir <vladimirleonov789@gmail.com>
2025-05-24 10:21:15 +0300

    refactor: project core files

--- apps/gateway/src/core/guards/optional-jwt-auth.guard.ts
+++ apps/gateway/src/core/guards/optional-jwt-auth.guard.ts
@@ -4,7 +4,6 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class OptionalJwtAuthGuard extends AuthGuard('optional-jwt') {
   handleRequest(err, user) {
-    console.log('handleRequest', err, user);
     if (err || !user) {
       return { userId: null };
     }
