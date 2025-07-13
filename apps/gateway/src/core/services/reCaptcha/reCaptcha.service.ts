545beaa7bfe7a1add47e074015fe7b3cfc5022ad
Vladimir <vladimirleonov789@gmail.com>
2025-07-13 12:42:32 +0300

    fix: recaptcha verify token body format

--- apps/gateway/src/core/services/reCaptcha/reCaptcha.service.ts
+++ apps/gateway/src/core/services/reCaptcha/reCaptcha.service.ts
@@ -6,26 +6,21 @@ export class ReCaptchaService {
   constructor(private captchaConfig: CaptchaConfig) {}
 
   async isValue(token: string): Promise<boolean> {
-    console.log('isValue token', token);
-    console.log(
-      'isValue this.captchaConfig.captchaSecret',
-      this.captchaConfig.captchaSecret,
-    );
-
     const result: Response = await fetch(
       'https://www.google.com/recaptcha/api/siteverify',
       {
         headers: {
-          'Content-Type': 'aplication/x-www-form-urlencoded',
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
         method: 'POST',
-        body: `secret=${this.captchaConfig.captchaSecret}&response=${token}`,
+        body: new URLSearchParams({
+          secret: this.captchaConfig.captchaSecret,
+          response: token,
+        }),
       },
     );
 
     const response: ReCaptchaResponse = await result.json();
-
-    console.log('isValue response result', response);
 
     if (!response.success) return false;
 
