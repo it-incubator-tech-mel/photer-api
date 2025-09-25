06233c55737be5b16df110db2f0b833341ef704d
Vladimir <vladimirleonov789@gmail.com>
2025-09-25 18:30:33 +0400

    refactor: refactor LoginDto validation

--- apps/gateway/src/features/auth/api/dto/input/login.dto.ts
+++ apps/gateway/src/features/auth/api/dto/input/login.dto.ts
@@ -9,20 +9,30 @@ import { Trim } from '../../../../../core/decorators/trim';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty()
+  @ApiProperty({
+    pattern: '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+    example: 'user@example.com',
+    description: 'must be unique',
+  })
   @Trim()
   @IsNotEmpty({ message: 'Email is required' })
-  @IsEmail({}, { message: 'Invalid email format' })
+  @IsEmail(
+    {},
+    { message: 'The email must match the format example@example.com' },
+  )
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    minLength: 6,
+    maxLength: 20,
+  })
   @IsString()
   @Trim()
   @IsNotEmpty({ message: 'Password is required' })
   @Length(6, 20, { message: 'Password must be between 6 and 20 characters' })
-  @Matches(/^[a-zA-Z0-9_-]+$/, {
+  @Matches(/^[0-9a-zA-Z!\"#$%&'()*+,\-./:;<=>?@\[\\\]^_{|}~]+$/, {
     message:
-      'Password can only contain letters, numbers, underscores, and hyphens',
+      'Password must contain 0-9, a-z, A-Z and may include special characters ! " # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _ { | } ~',
   })
   password: string;
 }
